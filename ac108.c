76a01405134d1ea6d1d9d369397a4f7666247340
Baozhu Zuo <zuobaozhu@gmail.com>
2017-10-09 15:00:40 +0800

    remove ac108 debug macro

--- ac108.c
+++ ac108.c
@@ -8,7 +8,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#define DEBUG 1
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
