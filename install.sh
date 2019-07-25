08f0d96d68f42eb9bfaa38e9d8316033f7950f1d
Baozhu Zuo <zuobaozhu@gmail.com>
2019-07-25 19:30:04 +0800

    raspberry pi 4 use the v7l+ suffix  as the kernel identifier

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ fi
 # it's just been updated)
 base_ver=$(get_kernel_version)
 base_ver=${base_ver%%[-+]*}
-kernels="${base_ver}+ ${base_ver}-v7+"
+kernels="${base_ver}+ ${base_ver}-v7+ ${base_ver}-v7l+"
 
 function install_module {
   local _i
