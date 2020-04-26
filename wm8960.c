2563020e294a64b266286f2863d28a279ca4f816
Hin-Tak Leung <htl10@users.sourceforge.net>
2020-04-26 23:27:54 +0100

    suppress "this statement may fall through" warning
    The semi-colon is needed, to prevent the comment being parsed as part of the "if {...}" statement.
    
    The warning looks like this:
    
    seeed-voicecard/wm8960.c: In function ‘wm8960_hw_params’:
    seeed-voicecard/wm8960.c:752:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
      752 |   if ((iface & 0x3) != 0) {
          |      ^
    seeed-voicecard/wm8960.c:757:2: note: here
      757 |  default:
          |  ^~~~~~~

--- wm8960.c
+++ wm8960.c
@@ -752,7 +752,8 @@ static int wm8960_hw_params(struct snd_pcm_substream *substream,
 		if ((iface & 0x3) != 0) {
 			iface |= 0x000c;
 			break;
-		}
+		};
+		/* fall through */
 	default:
 		dev_err(codec->dev, "unsupported width %d\n",
 			params_width(params));
